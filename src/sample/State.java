7133a31f400de3f6ae758026c9ca85f25071d580
Pietruszewski Mateusz <215851@edu.p.lodz.pl>
2020-04-12 14:18:19 +0200

    First app view

---
+++ src/sample/State.java
@@ -0,0 +1,39 @@
+package sample;
+
+public class State {
+    private String state;
+    private double baseTax;
+    private Category category;
+
+    public State(String state, double baseTax, Category category) {
+        this.state = state;
+        this.baseTax = baseTax;
+        this.category = category;
+    }
+
+    public double calcualteOfTax(double price, double category) {
+        if (category == 0)
+            return 0;
+
+        return price * (category / 100);
+    }
+
+    public double calcualteAfterTax(double price, double category) {
+        if (category == 0)
+            return price;
+
+        return price * (1 + (category / 100));
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public double getBaseTax() {
+        return baseTax;
+    }
+}
