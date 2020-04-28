edad9bdbc1cc0f7ecf3c0c6fc7fae8506d86fd69
Pietruszewski Mateusz <215851@edu.p.lodz.pl>
2020-04-28 22:43:04 +0200

    Created calculation and changed view

--- src/main/java/application/Controller.java
+++ src/main/java/application/Controller.java
@@ -1,34 +1,38 @@
 package application;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextArea;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import org.controlsfx.control.textfield.TextFields;
 
+import java.net.URL;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
-public class Controller {
-    List<State> states = new ArrayList<>();
-    ObservableList<String> observableListState = FXCollections.observableArrayList();
-    ObservableList<String> observableListCategory = FXCollections.observableArrayList();
+public class Controller implements Initializable {
+    private List<State> states = new ArrayList<>();
+    private List<ProductInfo> productInfoList = new ArrayList<>();
+    private List<String> productListAutoCompletion = new ArrayList<>();
+    private List<String> stateListAutoCompletion = new ArrayList<>();
 
-    @FXML
-    private ChoiceBox choiceBoxState;
-    @FXML
-    private ChoiceBox choiceBoxCategory;
-    @FXML
-    private TextArea priceOfTax, priceAfterTax;
-    @FXML
-    private TextField inputPrice;
     @FXML
     private Text warning;
+    @FXML
+    private TextField searchProduct;
+    @FXML
+    private TextField searchState;
+    @FXML
+    private TextField wholesalePriceTextField;
+    @FXML
+    private TextField marginTextField;
+    @FXML
+    private TextField priceWithoutTaxTextField;
 
-    public Controller() {
+    @FXML
+    public void initialize(URL location, ResourceBundle resources) {
         states.add(new State("Alabama", 4, new Category(4, 4, 0, 4, 4, 4)));
         states.add(new State("Alaska", 0, new Category(0, 0, 0, 0, 0, 0)));
         states.add(new State("Arizona", 5.6, new Category(0, 5.6, 0, 5.6, 5.6, 5.6)));
@@ -37,66 +41,58 @@ public class Controller {
         states.add(new State("Colorado", 2.9, new Category(0, 2.9, 0, 2.9, 2.9, 2.9)));
         states.add(new State("Connecticut", 6.35, new Category(0, 6.35, 0, 0, 6.35, 1)));
         states.add(new State("Delaware", 0, new Category(0, 0, 0, 0, 0, 0)));
-        states.forEach(x -> observableListState.add(x.getState()));
-    }
-
-    @FXML
-    private void initialize() {
-        observableListCategory.addAll("Groceries", "Prepared food", "Prescription drug", "Non-prescription drug", "Clothing", "Intangibles");
-        choiceBoxState.setItems(observableListState);
-        choiceBoxCategory.setItems(observableListCategory);
+        states.forEach(x -> stateListAutoCompletion.add(x.getState()));
+        productInfoList.add(new ProductInfo("apple", "groceries", 0.24));
+        productInfoList.add(new ProductInfo("orange", "groceries", 0.35));
+        productInfoList.add(new ProductInfo("pineapple", "groceries", 0.78));
+        productInfoList.add(new ProductInfo("Oxycodone", "Non-prescription-drug", 16.99));
+        productInfoList.add(new ProductInfo("Fentanyl", "Non-prescription-drug", 13.58));
+        productInfoList.forEach(x -> productListAutoCompletion.add(x.getProduct()));
+        TextFields.bindAutoCompletion(searchProduct, productListAutoCompletion);
+        TextFields.bindAutoCompletion(searchState, stateListAutoCompletion);
     }
 
     public void onClickButton() {
-
-        String choiceBoxStateValue = (String) choiceBoxState.getValue();
-        String choiceBoxCategoryValue = (String) choiceBoxCategory.getValue();
-
-        if (choiceBoxStateValue != null && choiceBoxCategoryValue != null) {
-            if (inputPrice.getText().isEmpty()) {
-                warning.setText("Enter price !");
-            } else if (!validatePrice(inputPrice.getText())) {
-                warning.setText("Incorrect price");
-            } else {
-                warning.setText("");
-                double price = Double.parseDouble(inputPrice.getText());
-                State state = findState(choiceBoxStateValue);
-                double resultOfTax = -1;
-                double resultAfterTax = -1;
-                switch (choiceBoxCategoryValue) {
-                    case "Groceries":
-                        resultOfTax = state.calcualteOfTax(price, state.getCategory().getGroceries());
-                        resultAfterTax = state.calcualteAfterTax(price, state.getCategory().getGroceries());
-                        break;
-                    case "Prepared food":
-                        resultOfTax = state.calcualteOfTax(price, state.getCategory().getPreparedFood());
-                        resultAfterTax = state.calcualteAfterTax(price, state.getCategory().getPreparedFood());
-                        break;
-                    case "Prescription drug":
-                        resultOfTax = state.calcualteOfTax(price, state.getCategory().getPrescriptionDrug());
-                        resultAfterTax = state.calcualteAfterTax(price, state.getCategory().getPrescriptionDrug());
-                        break;
-                    case "Non-prescription drug":
-                        resultOfTax = state.calcualteOfTax(price, state.getCategory().getNonPrescriptionDrug());
-                        resultAfterTax = state.calcualteAfterTax(price, state.getCategory().getNonPrescriptionDrug());
-                        break;
-                    case "Clothing":
-                        resultOfTax = state.calcualteOfTax(price, state.getCategory().getClothing());
-                        resultAfterTax = state.calcualteAfterTax(price, state.getCategory().getClothing());
-                        break;
-                    case "Intangibles":
-                        resultOfTax = state.calcualteOfTax(price, state.getCategory().getIntangibles());
-                        resultAfterTax = state.calcualteAfterTax(price, state.getCategory().getIntangibles());
-                        break;
+        String priceWholeString = wholesalePriceTextField.getText();
+        String stateString = searchState.getText();
+        String productString = searchProduct.getText();
+        if (priceWholeString.isEmpty() || stateString.isEmpty() || productString.isEmpty())
+            warning.setText("Enter all data");
+        else {
+            if (validatePrice(priceWholeString)) {
+                double WholesalePrice = Double.parseDouble(priceWholeString);
+                ProductInfo productInfo = findProduct(productString);
+                if (productInfo == null)
+                    warning.setText("Product not found");
+                else {
+                    State state = findState(stateString);
+                    if (state == null)
+                        warning.setText("State not found");
+                    else {
+                        warning.setText("");
+                        double PriceWithoutTax = calculateWithoutTax(WholesalePrice, productInfo, state);
+                        priceWithoutTaxTextField.setText(new DecimalFormat("##.##").format(PriceWithoutTax));
+                        Double margin = calculateMargin(PriceWithoutTax, productInfo);
+                        marginTextField.setText(new DecimalFormat("##.##").format(margin));
+                    }
                 }
-                priceOfTax.setText(new DecimalFormat("##.##").format(resultOfTax));
-                priceAfterTax.setText(new DecimalFormat("##.##").format(resultAfterTax));
+            } else {
+                warning.setText("Incorrect price");
             }
-        } else
-            warning.setText("Choose State and Category");
+        }
+    }
+
+    private double calculateWithoutTax(double WholesalePrice, ProductInfo productInfo, State state) {
+        return WholesalePrice / ((100.0 + getTaxCategory(productInfo, state)) / 100);
+    }
+
+    private double calculateMargin(double priceWithoutTax, ProductInfo productInfo) {
+        return priceWithoutTax - productInfo.getWholesalePrice();
     }
 
     private boolean validatePrice(String price) {
+        if (price.equals("."))
+            return false;
         boolean isDotted = false;
         for (int i = 0; i < price.length(); i++) {
             if (!Character.isDigit(price.charAt(i))) {
@@ -112,13 +108,44 @@ public class Controller {
         return true;
     }
 
-    private State findState(String choiceBoxValue) {
-        State state = null;
+    private State findState(String state) {
         for (State s : states) {
-            if (s.getState().equals(choiceBoxValue))
-                state = s;
+            if (s.getState().equals(state))
+                return s;
         }
-        return state;
+        return null;
     }
 
+    private ProductInfo findProduct(String product) {
+        for (ProductInfo productInfo : productInfoList) {
+            if (productInfo.getProduct().equals(product))
+                return productInfo;
+        }
+        return null;
+    }
+
+    private double getTaxCategory(ProductInfo productInfo, State state) {
+        double category = -1;
+        switch (productInfo.getCategory()) {
+            case "groceries":
+                category = state.getCategory().getGroceries();
+                break;
+            case "preparedFood":
+                category = state.getCategory().getPreparedFood();
+                break;
+            case "prescription-drug":
+                category = state.getCategory().getPrescriptionDrug();
+                break;
+            case "Non-prescription-drug":
+                category = state.getCategory().getNonPrescriptionDrug();
+                break;
+            case "clothing":
+                category = state.getCategory().getClothing();
+                break;
+            case "intangibles":
+                category = state.getCategory().getIntangibles();
+                break;
+        }
+        return category;
+    }
 }
--- src/main/java/application/State.java
+++ src/main/java/application/State.java
@@ -11,19 +11,6 @@ public class State {
         this.category = category;
     }
 
-    public double calcualteOfTax(double price, double category) {
-        if (category == 0)
-            return 0;
-
-        return price * (category / 100);
-    }
-
-    public double calcualteAfterTax(double price, double category) {
-        if (category == 0)
-            return price;
-
-        return price * (1 + (category / 100));
-    }
 
     public Category getCategory() {
         return category;
