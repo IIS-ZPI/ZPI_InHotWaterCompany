848325d433eaec42f22140a51a53a62be29fb11e
Pietruszewski Mateusz <215851@edu.p.lodz.pl>
2020-05-12 15:13:30 +0200

    Created function and view of margin in other state

--- src/main/java/application/Controller.java
+++ src/main/java/application/Controller.java
@@ -1,9 +1,13 @@
 package application;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
 import org.controlsfx.control.textfield.TextFields;
 
@@ -14,7 +18,7 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-    private List<State> states = new ArrayList<>();
+    private List<State> stateList = new ArrayList<>();
     private List<ProductInfo> productInfoList = new ArrayList<>();
     private List<String> productListAutoCompletion = new ArrayList<>();
     private List<String> stateListAutoCompletion = new ArrayList<>();
@@ -33,18 +37,24 @@ public class Controller implements Initializable {
     private TextField priceWithoutTaxTextField;
     @FXML
     private TextField wholesalePriceTextField;
+    @FXML
+    private TableView<MarginForAllState> tableView;
+    @FXML
+    private TableColumn<MarginForAllState, String> stateColumn;
+    @FXML
+    private TableColumn<MarginForAllState, Double> marginColumn;
 
     @FXML
     public void initialize(URL location, ResourceBundle resources) {
-        states.add(new State("Alabama", 4, new Category(4, 4, 0, 4, 4, 4)));
-        states.add(new State("Alaska", 0, new Category(0, 0, 0, 0, 0, 0)));
-        states.add(new State("Arizona", 5.6, new Category(0, 5.6, 0, 5.6, 5.6, 5.6)));
-        states.add(new State("Arkansas", 6.5, new Category(0.125, 6.5, 0, 6.5, 6.5, 0)));
-        states.add(new State("California", 7.25, new Category(0, 7.25, 0, 7.25, 7.25, 0)));
-        states.add(new State("Colorado", 2.9, new Category(0, 2.9, 0, 2.9, 2.9, 2.9)));
-        states.add(new State("Connecticut", 6.35, new Category(0, 6.35, 0, 0, 6.35, 1)));
-        states.add(new State("Delaware", 0, new Category(0, 0, 0, 0, 0, 0)));
-        states.forEach(x -> stateListAutoCompletion.add(x.getState()));
+        stateList.add(new State("Alabama", 4, new Category(4, 4, 0, 4, 4, 4)));
+        stateList.add(new State("Alaska", 0, new Category(0, 0, 0, 0, 0, 0)));
+        stateList.add(new State("Arizona", 5.6, new Category(0, 5.6, 0, 5.6, 5.6, 5.6)));
+        stateList.add(new State("Arkansas", 6.5, new Category(0.125, 6.5, 0, 6.5, 6.5, 0)));
+        stateList.add(new State("California", 7.25, new Category(0, 7.25, 0, 7.25, 7.25, 0)));
+        stateList.add(new State("Colorado", 2.9, new Category(0, 2.9, 0, 2.9, 2.9, 2.9)));
+        stateList.add(new State("Connecticut", 6.35, new Category(0, 6.35, 0, 0, 6.35, 1)));
+        stateList.add(new State("Delaware", 0, new Category(0, 0, 0, 0, 0, 0)));
+        stateList.forEach(x -> stateListAutoCompletion.add(x.getState()));
         productInfoList.add(new ProductInfo("apple", "groceries", 0.24));
         productInfoList.add(new ProductInfo("orange", "groceries", 0.35));
         productInfoList.add(new ProductInfo("pineapple", "groceries", 0.78));
@@ -53,34 +63,43 @@ public class Controller implements Initializable {
         productInfoList.forEach(x -> productListAutoCompletion.add(x.getProduct()));
         TextFields.bindAutoCompletion(searchProductTextField, productListAutoCompletion);
         TextFields.bindAutoCompletion(searchStateTextField, stateListAutoCompletion);
+
+        stateColumn.setCellValueFactory(new PropertyValueFactory<>("state"));
+        marginColumn.setCellValueFactory(new PropertyValueFactory<>("margin"));
     }
 
     public void onClickCheckButton() {
         String priceString = priceTextField.getText();
         String stateString = searchStateTextField.getText();
         String productString = searchProductTextField.getText();
-        if (priceString.isEmpty() || stateString.isEmpty() || productString.isEmpty())
+        if (priceString.isEmpty() || stateString.isEmpty() || productString.isEmpty()) {
             warning.setText("Enter all data");
-        else {
+            clearAllOutputField();
+        } else {
             if (validatePrice(priceString)) {
                 double price = Double.parseDouble(priceString);
                 ProductInfo productInfo = findProduct(productString);
-                if (productInfo == null)
+                if (productInfo == null) {
                     warning.setText("Product not found");
-                else {
+                    clearAllOutputField();
+                } else {
                     State state = findState(stateString);
-                    if (state == null)
+                    if (state == null) {
                         warning.setText("State not found");
-                    else {
+                        clearAllOutputField();
+                    } else {
                         warning.setText("");
                         double priceWithoutTax = calculateWithoutTax(price, productInfo, state);
                         priceWithoutTaxTextField.setText(new DecimalFormat("##.##").format(priceWithoutTax));
                         Double margin = calculateMargin(priceWithoutTax, productInfo);
                         marginTextField.setText(new DecimalFormat("##.##").format(margin));
+
+                        tableView.setItems(getMarginForAllstateList(stateList, productInfo, price));
                     }
                 }
             } else {
                 warning.setText("Incorrect price");
+                clearAllOutputField();
             }
         }
     }
@@ -122,7 +141,7 @@ public class Controller implements Initializable {
     }
 
     private State findState(String state) {
-        for (State s : states) {
+        for (State s : stateList) {
             if (s.getState().toLowerCase().equals(state.toLowerCase()))
                 return s;
         }
@@ -167,13 +186,31 @@ public class Controller implements Initializable {
     public void onClickCheckProductWholesalePrice() {
         String productString = searchProductTextField.getText();
         ProductInfo productInfo = findProduct(productString);
-        if (productInfo != null){
+        if (productInfo != null) {
             wholesalePriceTextField.setStyle("-fx-text-fill: black");
             wholesalePriceTextField.setText(String.valueOf(productInfo.getWholesalePrice()));
-        }
-        else{
+        } else {
             wholesalePriceTextField.setStyle("-fx-text-fill: red");
             wholesalePriceTextField.setText("Not found");
         }
+    }
+
+    public ObservableList<MarginForAllState> getMarginForAllstateList(List<State> stateList, ProductInfo productInfo, double price) {
+        ObservableList<MarginForAllState> marginForAllStateObservableList = FXCollections.observableArrayList();
+
+        for (State s : stateList) {
+            double priceWithoutTax = calculateWithoutTax(price, productInfo, s);
+            double margin = calculateMargin(priceWithoutTax, productInfo);
+            marginForAllStateObservableList.add(new MarginForAllState(s.getState(), new DecimalFormat("##.##").format(margin)));
+        }
+
+        return marginForAllStateObservableList;
+    }
+
+    void clearAllOutputField() {
+        marginTextField.setText("");
+        priceWithoutTaxTextField.setText("");
+        wholesalePriceTextField.setText("");
+        tableView.setItems(null);
     }
 }
