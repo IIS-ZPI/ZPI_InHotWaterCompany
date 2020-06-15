a1f3e5b24e25a7957ff19b1402107735163f19fe
Przemysław Frąg <215719@edu.p.lodz.pl>
2020-06-15 17:35:08 +0200

    Changed data source of exchange rates
    Now available over 150 currencies

--- src/main/java/utils/ExchangeRate.java
+++ src/main/java/utils/ExchangeRate.java
@@ -4,45 +4,67 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public final class ExchangeRate {
-	private static JSONObject data = null;
+	private static HashMap<String, Double> data = new HashMap<>();
 	
 	private ExchangeRate() {
 		throw new IllegalStateException("Utility class");
 	}
 	
 	private static void fetchData() throws IOException {
-		URL url = new URL("https://api.exchangeratesapi.io/latest?base=USD");
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openConnection().getInputStream()));
-		StringBuilder builder = new StringBuilder();
+		URL[] urls = {
+			new URL("https://api.nbp.pl/api/exchangerates/tables/A/?format=json"),
+			new URL("https://api.nbp.pl/api/exchangerates/tables/B/?format=json")
+		};
 		
-		String line = bufferedReader.readLine();
-		
-		while (line != null) {
-			builder.append(line);
-			line = bufferedReader.readLine();
+		for (URL url : urls) {
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openConnection().getInputStream()));
+			StringBuilder builder = new StringBuilder();
+			
+			String line = bufferedReader.readLine();
+			
+			while (line != null) {
+				builder.append(line);
+				line = bufferedReader.readLine();
+			}
+			
+			bufferedReader.close();
+			
+			try {
+				JSONArray rates = new JSONArray(builder.toString()).getJSONObject(0).getJSONArray("rates");
+				
+				for (int i = 0; i < rates.length(); i++) {
+					JSONObject json = rates.getJSONObject(i);
+					data.put(json.getString("code"), json.getDouble("mid"));
+				}
+				
+			} catch (JSONException e) {
+				throw new IOException("Incorrect response form server");
+			}
 		}
 		
-		bufferedReader.close();
+		data.put("PLN", 1.0);
 		
-		try {
-			data = new JSONObject(builder.toString()).getJSONObject("rates");
-			
-		} catch (JSONException e) {
-			throw new IOException("Incorrect response form server");
+		double usd = data.get("USD");
+		
+		for (Entry<String, Double> entry : data.entrySet()) {
+			entry.setValue(usd / entry.getValue());
 		}
 	}
 	
 	/**
-	 * Returns exchange rate for specified European country currency.
+	 * Returns exchange rate for specified currency.
 	 * Rates are quoted against the USD.
 	 * At first call, data is downloaded from remote server and cached to
 	 * avoid excessive network traffic and speed up next calls.
-	 * Data source: https://exchangeratesapi.io/
+	 * Data source: https://api.nbp.pl/
 	 * 
 	 * @param currencyCode - ISO 4217 currency code
 	 * @return rate against to USD
@@ -50,14 +72,14 @@ public final class ExchangeRate {
 	 * @throws IllegalArgumentException if specified currency isn't supported
 	 */
 	public static double getFor(String currencyCode) throws IOException {
-		if (data == null) {
+		if (data.isEmpty()) {
 			fetchData();
 		}
 		
-		if (!data.has(currencyCode)) {
+		if (!data.containsKey(currencyCode)) {
 			throw new IllegalArgumentException("Unsupported currency");
 		}
 		
-		return data.getDouble(currencyCode);
+		return data.get(currencyCode);
 	}
 }
